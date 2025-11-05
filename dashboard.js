6300fd1739d20462529b431715ac9c3b1241fd6f
Abinaya8125 <abinayakarpagam8125@gmail.com>
2025-11-05 08:25:53 -0800

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md text-center">
+      <h2 className="text-2xl font-bold mb-4">Welcome to Dashboard!</h2>
+      <p>You have successfully logged in.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
